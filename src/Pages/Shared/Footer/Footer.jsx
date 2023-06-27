4f923a0df1baacb406a93e2b7a518dbaa10a91cd
Wasey Rahman <wasey123435@gmail.com>
2023-06-27 14:42:53 +0600

    Footer

---
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer footer-center p-4 bg-pink-700 text-base-content">
+  <div>
+    <p className="text-white">Copyright © 2023 - All right reserved by Wasey Rahman Portfolio</p>
+  </div>
+</footer>
+        </div>
+    );
+};
+
+export default Footer;

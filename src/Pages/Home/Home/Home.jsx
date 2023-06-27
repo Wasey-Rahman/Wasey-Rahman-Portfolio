67f1744fea3cb0e0e122ef83c680051c4f2b4476
Wasey Rahman <wasey123435@gmail.com>
2023-06-27 21:41:11 +0600

    About Me

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import photo from './Photo of Wasey Rahman for CV.jpg';
+import AboutMe from '../AboutMe/AboutMe';
 
 const Home = () => {
   const [showAnimation, setShowAnimation] = useState(false);
@@ -46,6 +47,7 @@ const Home = () => {
         </motion.div>
      
         </div>
+        <AboutMe></AboutMe>
     </div>
   );
 };
